3d4d29fb84e9f5815c5f19d806d537bd4557c6b1
Andrea <andrea@campo23.(none)>
2013-03-15 22:08:28 +0100

    new tipe of button

--- Button.py
+++ Button.py
@@ -2,15 +2,23 @@ import pygame
 from pygame.locals import *
 
 class Button:
-    def __init__(self, color, position, size):
-        self.color = color
+    def __init__(self, position, size):
+        self.colorIn = (124, 252, 0)
+        self.colorMid = (0, 128, 0)
+        self.colorEx = (50, 205, 50)
         self.position = position
         self.size = size
-        self.button = pygame.Rect(self.position[0], self.position[1], self.size[0], self.size[1])
+        self.buttonIn = pygame.Rect(self.position[0], self.position[1], self.size[0], self.size[1])
+        self.buttonMid = pygame.Rect(self.position[0]-3, self.position[1]-3, self.size[0]+6, self.size[1]+6)
+        self.buttonEx = pygame.Rect(self.position[0]-6, self.position[1]-6, self.size[0]+12, self.size[1]+12)
         self.mouse_pos = [-1, -1]    
         
     def draw(self, screen):
-        pygame.draw.rect(screen, self.color, self.button)
+        pygame.draw.rect(screen, self.colorEx, self.buttonEx)
+        pygame.draw.rect(screen, self.colorMid, self.buttonMid)
+        pygame.draw.rect(screen, self.colorIn, self.buttonIn)
+        
+        
     def isClicked(self): 
         if(pygame.mouse.get_pressed() == (1, 0, 0)):
             self.mouse_pos = pygame.mouse.get_pos()
